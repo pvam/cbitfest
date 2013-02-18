1c68d83945cfc8084131621f6690b99e64cb1166
saisreenivasan <sreenivasanprativadhi@gmail.com>
2013-02-18 19:36:05 +0530

    handle check

--- reginsert.php
+++ reginsert.php
@@ -1,4 +1,11 @@
+<pre>
 <?php
+error_reporting(E_ALL); // or E_STRICT
+ini_set("display_errors",1);
+//ini_set("memory_limit","1024M");
+//print_r($_FILES['photo']);
+print_r($_POST);
+//print_r($_POST['photo']);
 	try {
     $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -7,14 +14,64 @@
    print( "Error connecting to SQL Server." );
    die(print_r($e));
    }
-
-	   $handle = $_GET['id'];
-	   $object = $_GET['object'];
-	   $insert = "INSERT into cbit(handle,object) VALUES (?,?)";
-		$stmt=$conn->prepare($insert);
-		$stmt->bindValue(1,$handle);
-		$stmt->bindValue(2,$object);
-		$stmt->execute();
-		echo "success";
-
-?>
+class User {
+    public $name = "";
+    public $email  = "";
+    public $handle = "";
+	public $imagepath;
+	public $password;
+public $dob ="";
+public $gender ="";
+}
+ try 
+	{
+		//This is the directory where images will be saved 
+        // $target = "userdata/"; 
+        // $target = $target.basename($_FILES['photo']['name']); 
+		 $c = uniqid (rand (),true);
+		 $ext = pathinfo($_FILES['photo']['name']);
+		 echo $ext;
+		 $extn = $ext['extension'];
+		 $c = $c .".".$extn;
+		 if(move_uploaded_file($_FILES['photo']['tmp_name'],$c)) 
+		 { 
+		 //Tells you if its all ok 
+		 echo "Image ". $_FILES['photo']['name']."has been uploaded to http://icards.azurewebsites.net/".$c; 
+		 } 
+		 else { 
+		 //Gives and error if its not 
+		 echo "Sorry, there was a problem uploading your file."; 
+		 echo "<br>".$_FILES['photo']['error'];
+		 } 
+		 $user = new User();
+		 //echo $_FILES["photo"]["name"];
+		  $user->name = $_POST["name"];
+		  $user->email  = $_POST["email"];
+		  $user->handle  = $_POST["handle"];
+		  $user->imagepath = $c;
+		  $user->password=$_POST["pwd"];
+		  $user->gender=$_POST["gender"];
+		  $user->dob=$_POST["dob"];
+		  $string=json_encode($user);
+		  echo $string;
+		  echo "<br>";
+		  $insert = "INSERT into cbit(object,handle) VALUES (?,?)";
+		  $stmt=$conn->prepare($insert);
+		  $stmt->bindValue(1,$string);
+		  $stmt->bindValue(2,$user->handle);
+		  $stmt->execute();
+		  echo "<br>inserted data";
+		  //Writes the photo to the server 
+		
+    }
+    catch(Exception $e)
+    {
+        //die( mssql_POST_last_message());
+		die(var_dump($e));
+		if($e->getCode()==23000)//code for primary key 23000
+		{
+		echo "primary key voilation :(";
+		}
+    }
+    ?>
+</pre>
--- registration.php
+++ registration.php
@@ -1,11 +1,4 @@
-<pre>
 <?php
-error_reporting(E_ALL); // or E_STRICT
-ini_set("display_errors",1);
-//ini_set("memory_limit","1024M");
-//print_r($_FILES['photo']);
-print_r($_POST);
-//print_r($_POST['photo']);
 	try {
     $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -14,64 +7,20 @@ print_r($_POST);
    print( "Error connecting to SQL Server." );
    die(print_r($e));
    }
-class User {
-    public $name = "";
-    public $email  = "";
-    public $handle = "";
-	public $imagepath;
-	public $password;
-public $dob ="";
-public $gender ="";
-}
- try 
+
+
+   $stmt1 ="Select * from cbit where handle=?";
+	$prp1 = $conn->prepare($stmt1);
+    if($prp1->execute(array($_GET['id'])) )
 	{
-		//This is the directory where images will be saved 
-        // $target = "userdata/"; 
-        // $target = $target.basename($_FILES['photo']['name']); 
-		 $c = uniqid (rand (),true);
-		 $ext = pathinfo($_FILES['photo']['name']);
-		 echo $ext;
-		 $extn = $ext['extension'];
-		 $c = $c .".".$extn;
-		 if(move_uploaded_file($_FILES['photo']['tmp_name'],$c)) 
-		 { 
-		 //Tells you if its all ok 
-		 echo "Image ". $_FILES['photo']['name']."has been uploaded to http://icards.azurewebsites.net/".$c; 
-		 } 
-		 else { 
-		 //Gives and error if its not 
-		 echo "Sorry, there was a problem uploading your file."; 
-		 echo "<br>".$_FILES['photo']['error'];
-		 } 
-		 $user = new User();
-		 //echo $_FILES["photo"]["name"];
-		  $user->name = $_POST["name"];
-		  $user->email  = $_POST["email"];
-		  $user->handle  = $_POST["handle"];
-		  $user->imagepath = $c;
-		  $user->password=$_POST["pwd"];
-		  $user->gender=$_POST["gender"];
-		  $user->dob=$_POST["dob"];
-		  $string=json_encode($user);
-		  echo $string;
-		  echo "<br>";
-		  $insert = "INSERT into cbit(object,handle) VALUES (?,?)";
-		  $stmt=$conn->prepare($insert);
-		  $stmt->bindValue(1,$string);
-		  $stmt->bindValue(2,$user->handle);
-		  $stmt->execute();
-		  echo "<br>inserted data";
-		  //Writes the photo to the server 
-		
-    }
-    catch(Exception $e)
-    {
-        //die( mssql_POST_last_message());
-		die(var_dump($e));
-		if($e->getCode()==23000)//code for primary key 23000
-		{
-		echo "primary key voilation :(";
-		}
-    }
-    ?>
-</pre>
+		$registrant = $prp1->fetchAll();
+      if(count($registrant) >0)
+	   {
+		echo "present";
+	   }
+	   else
+	    echo "absent";
+
+	}
+ 
+?>
