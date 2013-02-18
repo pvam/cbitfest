08e983bb9f41aac39487bed7dd2273398981c923
saisreenivasan <sreenivasanprativadhi@gmail.com>
2013-02-18 18:06:29 +0530

    display page added

---
+++ js/display.js
@@ -0,0 +1,35 @@
+document.getElementById("fname").innerHTML = "Sai Sreenivasan";
+var id = getCookie("username");//handle
+	
+//to get the cookie value(username)	in this page
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+  {
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+
+
+if(id!=null||id!="") //checking if hes logged in or not
+{
+var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'js/display.php?id='+id,false);
+        xhr.send(null);
+    var encoded = xhr.responseText;
+console.log(encoded);
+decoded = JSON.parse(encoded);
+
+document.getElementById("fname").innerHTML = decoded.firstname;
+document.getElementById("gender").innerHTML = decoded.gender;
+document.getElementById("email").innerHTML = decoded.email;
+document.getElementById("dob").innerHTML = decoded.dob;
+}
---
+++ js/display.php
@@ -0,0 +1,20 @@
+<?php
+	try {
+    $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+   catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+   }
+	$sql_select = "SELECT * FROM cbit where handle= ?";
+	$prp = $conn->prepare($sql_select);
+    if($prp->execute(array($_GET['id'])) )
+	{
+		   $registrant = $prp->fetch();
+			echo $registrant['object'];
+    } 
+	else
+	{
+		echo "error";
+	}?>
